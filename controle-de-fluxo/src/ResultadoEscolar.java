d910e35a1ede54bf946535de6548c14261113ac8
Yan da Costa Fernandes Gomes <143640083+yancfgomes@users.noreply.github.com>
2024-06-09 01:00:01 -0400

    ResultadoEscolar utilizando condição ternária

--- controle-de-fluxo/src/ResultadoEscolar.java
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -1,12 +1,7 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
     int nota = 6;
-
-	if (nota >= 7)
-		System.out.println("Aprovado");
-	else if (nota >= 5 && nota < 7)
-		System.out.println("Recuperação");
-	else
-		System.out.println("Reprovado"); 
+    String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+    System.out.println(resultado); 
     }
 }
