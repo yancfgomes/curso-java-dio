d2b4b3ac74e5e63dc373c8e45f2de69fc9dd7e5e
Yan da Costa Fernandes Gomes <143640083+yancfgomes@users.noreply.github.com>
2024-06-10 10:59:14 -0400

    create interface IConta

--- POO/desafio2-poo/bancoDigital/src/main/java/com/example/banco/Conta.java
+++ POO/desafio2-poo/bancoDigital/src/main/java/com/example/banco/Conta.java
@@ -1,13 +1,26 @@
 package main.java.com.example.banco;
 
-public class Conta {
+public abstract class Conta implements IConta{
     private int agencia;
     private int numero;
     private double saldo;
 
-    public void sacar(){}
-    public void depositar(){}
-    public void transferir(){}
+    
+
+    @Override
+    public void sacar(double valor) {
+        
+    }
+    @Override
+    public void depositar(double valor) {
+        
+    }
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+        
+    }
+
+
     public int getAgencia() {
         return agencia;
     }
---
+++ POO/desafio2-poo/bancoDigital/src/main/java/com/example/banco/IConta.java
@@ -0,0 +1,8 @@
+package main.java.com.example.banco;
+
+public interface IConta {
+    void sacar(double valor);
+    void depositar(double valor);
+    void transferir(double valor, Conta contaDestino);
+    
+}
