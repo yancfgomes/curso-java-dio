5a0c35fe8f6bbdcbbafce7083f8e4fcb31fc198d
Yan da Costa Fernandes Gomes <143640083+yancfgomes@users.noreply.github.com>
2024-06-09 21:15:49 -0400

    new project banco-digital

---
+++ POO/desafio2-poo/bancoDigital/src/main/java/com/example/banco/Cliente.java
@@ -0,0 +1,13 @@
+package main.java.com.example.banco;
+
+public class Cliente {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
